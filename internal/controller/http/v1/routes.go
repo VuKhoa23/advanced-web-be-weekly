547b675c9eb309685937810f0f71736025d8caf1
Hòa Đặng <100982269+hoadang0305@users.noreply.github.com>
2024-10-21 11:42:29 +0700

    Hoa/logging request (#14) - fix reading body twice
    * demo logging for request
    
    * fix bug cannot handler request have no body
    
    * handler for request and response
    
    * feat: update logging with logrus and logdy client
    
    * refactor: fix naming for mdws
    
    * handler for both success response and error response
    
    * fix: fix reading body twice err
    
    ---------
    
    Co-authored-by: VuKhoa23 <vuanhkhoa007@gmail.com>

--- internal/controller/http/v1/routes.go
+++ internal/controller/http/v1/routes.go
@@ -2,30 +2,37 @@ package v1
 
 import (
 	"bytes"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
 	"github.com/natefinch/lumberjack"
 	"github.com/sirupsen/logrus"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-	"time"
+	"io"
 )
 
 func LoggingRequestMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var bodyData interface{}
-		if err := c.ShouldBindBodyWith(&bodyData, binding.JSON); err != nil {
-			print(err.Error())
-			if err.Error() == "EOF" {
-				bodyData = ""
-			}
+		bodyBytes, _ := io.ReadAll(c.Request.Body)
+		err := c.Request.Body.Close()
+		if err != nil {
+			c.Next()
+			return
 		}
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+		var bodyJSON map[string]interface{}
+		err = json.Unmarshal(bodyBytes, &bodyJSON)
+		if err != nil {
+			c.Next()
+			return
+		}
+
 		logrus.WithFields(logrus.Fields{
 			"method": c.Request.Method + "-request",
 			"path":   c.Request.URL.Path,
 			"query":  c.Request.URL.RawQuery,
-			"body":   bodyData,
-			"time":   time.Now(),
+			"body":   bodyJSON,
 		}).Info()
 		c.Next()
 	}
@@ -48,16 +55,26 @@ func LoggingResponseMiddleware() gin.HandlerFunc {
 		c.Next()
 		statusCode := c.Writer.Status()
 		if statusCode >= 0 {
-			logrus.WithFields(logrus.Fields{
-				"method": c.Request.Method + "-response",
-				"path":   c.Request.URL.Path,
-				"query":  c.Request.URL.RawQuery,
-				"body":   blw.body.String(),
-				"time":   time.Now(),
-			}).Info()
+			if statusCode < 400 {
+				logrus.WithFields(logrus.Fields{
+					"method": c.Request.Method + "-response",
+					"path":   c.Request.URL.Path,
+					"query":  c.Request.URL.RawQuery,
+					"body":   blw.body.String(),
+				}).Info()
+			} else {
+				logrus.WithFields(logrus.Fields{
+					"method": c.Request.Method + "-response",
+					"path":   c.Request.URL.Path,
+					"query":  c.Request.URL.RawQuery,
+					"body":   blw.body.String(),
+				}).Error()
+			}
+
 		}
 	}
 }
+
 func MapRoutes(router *gin.Engine, actorHandler *ActorHandler, filmHandler *FilmHandler) {
 	logrus.SetOutput(&lumberjack.Logger{
 		Filename:   "app.log",
